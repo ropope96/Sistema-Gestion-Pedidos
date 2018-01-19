afac918735c0eae19abb680d896e6862ea1b2297
Roberto <roberto_poicon@usmp.pe>
2018-01-18 22:09:48 -0500

    Primer avance

---
+++ SistemaGestionPedidos/Controllers/PedidoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace SistemaGestionPedidos.Controllers
+{
+	public class PedidoController : Controller
+	{
+		// GET: Pedido 
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult GenerarPedido()
+		{
+			return View();
+		}
+	}
+}
---
+++ SistemaGestionPedidos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaGestionPedidos.MvcApplication" Language="C#" %>
